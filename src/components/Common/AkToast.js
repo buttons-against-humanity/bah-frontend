17e4888c8d1766a357ec5fc10e1140b65bc8c9a5
Michele Azzolari <michele@azzolari.it>
2020-04-12 11:18:28 +0200

    AkToast: fix bug when multiple toasts appear

--- src/components/Common/AkToast.js
+++ src/components/Common/AkToast.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let tout;
+let toastCounter = 0;
 
 const AKToast = (message, autohide, error = false) => {
+  let tout;
   const elementRootName = document.createElement('div');
   document.body.appendChild(elementRootName);
 
@@ -11,16 +12,21 @@ const AKToast = (message, autohide, error = false) => {
     if (tout) {
       clearTimeout(tout);
     }
+    toastCounter--;
     ReactDOM.unmountComponentAtNode(elementRootName);
     elementRootName.remove();
   };
   const toast = (
-    <div className="x-toast-wrapper" style={{ position: 'fixed', top: 10, right: 10, zIndex: 9999 }}>
+    <div
+      className="x-toast-wrapper"
+      style={{ position: 'fixed', top: toastCounter * 150 + 10, right: 10, zIndex: 9999 }}
+    >
       <div className="x-toast m-2" style={{ opacity: 1 }}>
         <div className={'alert p-4 alert-' + (error ? 'danger' : 'info')}>{message}</div>
       </div>
     </div>
   );
+  toastCounter++;
   ReactDOM.render(toast, elementRootName);
   tout = setTimeout(unrender, autohide || 2000);
 };
