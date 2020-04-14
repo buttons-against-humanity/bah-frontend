259a7f4796f254c57dea01c6ee39be8133513fac
Michele Azzolari <michele@azzolari.it>
2020-04-14 09:42:42 +0200

    Rebranding

--- src/components/Game/Homepage.js
+++ src/components/Game/Homepage.js
@@ -12,24 +12,24 @@ class Homepage extends PureComponent {
       <div className="container text-center">
         <div className="alert alert-dark mt-4 text-left">
           <p>
-            Cards Against Humanity is a party game for horrible people. <br />
-            Unlike most of the party games you've played before, Cards Against Humanity is as despicable and awkward as
-            you and your friends.
+            Buttons Against Humanity is a party game for horrible people. <br />
+            Unlike most of the party games you've played before, Buttons Against Humanity is as despicable and awkward
+            as you and your friends.
             <br />
             <br />
-            The game is simple. Each round, one player asks a question from a black card, and everyone else answers with
-            their funniest white card.
+            The game is simple. Each round, a question is shown, and everyone - but one, called "the button czar" -
+            answers with their funniest button. The Button Czar has the hard work to choose the round's winner.
           </p>
           <p className="mt-5">
-            This is a online fork of{' '}
+            This is a online clone of{' '}
             <a href="https://www.cardsagainsthumanity.com/" target="_blank" rel="noopener noreferrer">
-              The Original Cards Against Humanity Game
+              Cards Against Humanity Game
             </a>
           </p>
           <p className="mt-5">
             Source code is available on{' '}
-            <a href="https://git.fluidware.it/pcah/" target="_blank" rel="noopener noreferrer">
-              Fluidware's Gitlab
+            <a href="https://github.com/buttons-against-humanity" target="_blank" rel="noopener noreferrer">
+              GitHub
             </a>{' '}
             with Apache License 2.0
           </p>
