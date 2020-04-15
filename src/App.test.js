809e17675f6b25732c399f693dba04873befae8f
Michele Azzolari <michele@azzolari.it>
2020-04-15 13:50:53 +0200

    CI: add test stage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import configureStore from './store';
+
+const initialState = {};
+const store = configureStore(initialState);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store} />, div);
 });
