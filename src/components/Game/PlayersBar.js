0cfe28cadf6ce5645d2fe635970b3c51d59c22fd
Michele Azzolari <michele@azzolari.it>
2020-05-11 06:46:55 +0200

    PlayerBar: wrap it into container-fluid

--- src/components/Game/PlayersBar.js
+++ src/components/Game/PlayersBar.js
@@ -16,30 +16,32 @@ class PlayersBar extends PureComponent {
       return null;
     }
     return (
-      <div className="row my-2">
-        <div className="col-12">
-          <span className="ml-2">
-            <strong>Players:</strong>
-          </span>
-          {players
-            .sort((a, b) => {
-              if (a.points > b.points) return -1;
-              if (a.points < b.points) return 1;
-              return 0;
-            })
-            .map((_player, i) => {
-              const card_czar = round && round.card_czar.uuid === _player.uuid;
-              return (
-                <span key={i} className={'ml-3 p-2 ' + (card_czar ? ' bg-dark text-white' : '')}>
-                  {_player.name} - Points: {_player.points}
-                </span>
-              );
-            })}
-          {round && rounds > 0 && (
-            <div className="float-right mr-3 p-2">
-              <strong>{isPlayoff ? 'Playoff' : `Round ${round.n} / ${rounds}`}</strong>
-            </div>
-          )}
+      <div className="container-fluid">
+        <div className="row my-2">
+          <div className="col-12">
+            <span className="ml-2">
+              <strong>Players:</strong>
+            </span>
+            {players
+              .sort((a, b) => {
+                if (a.points > b.points) return -1;
+                if (a.points < b.points) return 1;
+                return 0;
+              })
+              .map((_player, i) => {
+                const card_czar = round && round.card_czar.uuid === _player.uuid;
+                return (
+                  <span key={i} className={'ml-3 p-2 ' + (card_czar ? ' bg-dark text-white' : '')}>
+                    {_player.name} - Points: {_player.points}
+                  </span>
+                );
+              })}
+            {round && rounds > 0 && (
+              <div className="float-right mr-3 p-2">
+                <strong>{isPlayoff ? 'Playoff' : `Round ${round.n} / ${rounds}`}</strong>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     );
